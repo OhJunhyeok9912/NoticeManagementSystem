38343d127dfe1f20a38421fa6573b0ea8c6b7a1e
OhJunhyeok9912 <joonlove1234@gmail.com>
2021-03-12 01:09:36 +0900

    MenuManager

---
+++ src/MenuManager.java
@@ -0,0 +1,11 @@
+
+public class MenuManager {
+    
+	 public static void main(String[] args) {
+		 System.out.println("1. Add Notice");
+		 System.out.println("2. Delete Notice");
+		 System.out.println("3. Edit Notice");
+		 System.out.println("4. View Notice");
+		 System.out.println("5. Exit");
+	 }
+}
